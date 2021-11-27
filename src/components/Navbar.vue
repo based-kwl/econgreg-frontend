b8956d829892b174e791d38058a6590438572185
PhunnieB <phunniehypnotix@gmail.com>
2021-11-27 17:37:30 -0500

    unfinished cringe

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,7 @@
   <header class="text-gray-400 bg-gdark body-font">
     <div class="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
       <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+        <img src="http://assets.greg.192.168.1.126.nip.io:8086/images/bw-helix.png" class="object-contain h-10 w-10">
         <router-link to="/" class="ml-3 text-xl">Econgreg</router-link>
       </a>
       <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <body class="bg-gdark bg-clip-content w-full h-screen">
-    <section class="text-gray-400 body-font" ref="vantaRef">
-      <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <section class="text-gray-400 body-font h-1/2" ref="vantaRef">
+      <div class="container mx-auto flex px-5 py-2 md:flex-row flex-col items-center">
         <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 class="title-font sm:text-4xl text-3xl mb-4 font-bold text-transparent bg-clip-text bg-gradient-to-br from-yellow-500 to-gray-200">SOME CATCHY HERO TITLE HERE IDK</h1>
-          <p class="mb-8 leading-relaxed text-gmilk">Something to explain the purpose of the site</p>
+          <img src="http://assets.greg.192.168.1.126.nip.io:8086/images/bw-helix-eg.png" class="object-contain h-1/5 w-1/5">
+          <p class="mb-8 text-xl leading-relaxed text-gmilk">Control a telescope without buying one.</p>
           <div class="flex w-full md:justify-start justify-center items-end">
             <div class="relative mr-4 md:w-full lg:w-full xl:w-1/2 w-2/4">
               <label for="hero-field" class="leading-7 text-sm text-gmilk">Ready to explore the skies?</label>
-              <input type="text" id="hero-field" placeholder="Twitch name" name="hero-field" class="w-full bg-gray-800 rounded bg-opacity-40 border border-gray-700 focus:ring-2 focus:ring-green-900 focus:bg-transparent focus:border-green-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+              <input type="text" id="hero-field" placeholder="Twitch name. E.g. Optoscope" name="hero-field" class="w-full bg-gray-800 rounded bg-opacity-40 border border-gray-700 focus:ring-2 focus:ring-green-900 focus:bg-transparent focus:border-green-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
               </div>
               <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -20,9 +20,12 @@
         </div>
       </div>
     </section>
-    <div class="w-full h-4/5 bg-gdark flex justify-center items-center">
+    <div class="w-full h-1/5 bg-gdark flex justify-center items-center mt-10">
+      <p class="text-9xl font-bold font-largeText text-gmilk">GALLERY</p>
+    </div>
+    <div class="w-full h-4/5 bg-gdark flex justify-center items-center -mt-24">
       <div class="bg-gdark w-4/5">
-        <vue-carousel class="h-full" :data="data" indicators="hover"></vue-carousel>
+        <vue-carousel class="h-full border-2 border-gmilk" :data="data" indicators="hover"></vue-carousel>
       </div>
     </div>
     
