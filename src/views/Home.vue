3208b52e65a295f9e742d98a3554c840fda771ff
PhunnieB <phunniehypnotix@gmail.com>
2021-11-27 04:11:18 -0500

    button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,9 +9,14 @@
             <div class="relative mr-4 md:w-full lg:w-full xl:w-1/2 w-2/4">
               <label for="hero-field" class="leading-7 text-sm text-gmilk">Ready to explore the skies?</label>
               <input type="text" id="hero-field" placeholder="Twitch name" name="hero-field" class="w-full bg-gray-800 rounded bg-opacity-40 border border-gray-700 focus:ring-2 focus:ring-green-900 focus:bg-transparent focus:border-green-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+              </div>
+              <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <span>Explore</span>
+              </button>
             </div>
-            <button class="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">Explore</button>
-          </div>
         </div>
       </div>
     </section>
